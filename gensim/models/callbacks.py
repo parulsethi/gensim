7620efca95c72ecc0acd3f71ba74f61850fe5b0a
parulsethi <parul1sethi@gmail.com>
2017-07-06 05:49:03 +0530

    add base structure

--- gensim/models/callbacks.py
+++ gensim/models/callbacks.py
@@ -4,7 +4,7 @@ import numpy as np
 from visdom import Visdom
 
 
-class CoherenceCallback(object):
+class Coherence(object):
     def __init__(self, model=None, topics=None, texts=None, corpus=None, dictionary=None, coherence=None, window_size=None, topn=None, logger=None, viz_env=None):
         self.model = model
         self.topics = topics
@@ -17,51 +17,70 @@ class CoherenceCallback(object):
         self.logger = logger
         self.viz_env = viz_env
 
-    def get_metric(self, topics=None):
+    def get_value(self, topics=None):
         if topics is None:
             topics = self.topics
         cm = gensim.models.CoherenceModel(self.model, topics, self.texts, self.corpus, self.dictionary, self.window_size, self.metric, self.topn)
         return cm.get_coherence()
 
+class Perplexity(object):
+	def __init__(self, arg):
+		self.arg = arg
+
+	def get_value(self, arg):
+
+		
+class DiffMetric(object):
+	def __init__():
+
+	def get_value(self):
+
+
+class Convergence(object):
+	def __init__(self, arg):
+		self.arg = arg
+
+	def get_value(self):
+		
+
+class Callback(object):
+	def __init__(self, metrics=None):
+		# list of metric callbacks
+		self.metrics = metrics
+
     def set_model(self, model):
         self.model = model
         if self.logger=='visdom':
-            self.viz_window = Visdom()
-            self.visualize = Visualize(self.viz_env, self.viz_window)
+            self.viz = Visdom()
+            self.windows = []
+            self.diff_mat = []
         if self.logger=='shell':
-            self.log = Log(type(self.model))
+        	model_type = str(model_type)
+            self.log_type = logging.getLogger(model_type)
 
     def on_epoch_end(self, epoch, topics=None):
         # provide topics after current epoch if coherence is not valid for this topic model
-        value = self.get_metric(topics=topics)
-        if self.logger=='visdom':
-            self.visualize.line_plot(value, epoch, 'Epochs', 'Coherence', 'Coherence (%s)' % self.metric)
-        if self.logger=='shell':
-            statement = "Coherence estimate: %.3f" % value
-            self.log.log_metric(statement)
+        for i, metric in enumerate(self.metrics):
+	        value = metric.get_value(topics=topics)
 
+		    if metric.logger=="visdom":
+			    if epoch==0:
+			    	if type(value)==int:
+				        viz_metric = self.viz.line(Y=np.array([value]), X=np.array([epoch]), env=metric.env, opts=dict(xlabel=xlabel, ylabel=ylabel, title=title))
+				        self.windows.append(copy.deepcopy(viz_metric))
+				    else:
+				    	self.diff_mat = np.array([value])
+				    	viz_metric = self.viz.heatmap(X=np.array(self.diff_mat).T, env=metric.env, opts=dict(xlabel='Epochs', ylabel='Topic', title='Diff (%s)' % diff_distance))
+			    		self.windows.append(copy.deepcopy(viz_metric))
+			    else:
+			    	if type(value)==int:
+			    		self.viz.updateTrace(Y=np.array([value]), X=np.array([epoch]), env=metric.env, win=self.windows[i])
+			    	else:
+			    		self.diff_mat = np.concatenate((self.diff_mat, np.array([value])))
+			    		self.viz.heatmap(X=np.array(self.diff_mat).T, env=viz_env, win=self.viz_diff, opts=dict(xlabel='Epochs', ylabel='Topic', title='Diff (%s)' % diff_distance))
 
-class Visualize(object):
-    def __init__(self, viz_env, viz_window):
-        self.env = viz_env
-        self.window = viz_window
+	        if metric.logger=='shell':
+	            statement = "Estimate: %.3f" % value
+	            self.log_type.info(statement)
 
-    def line_plot(self, value, epoch, xlabel, ylabel, title):
-        value = np.array([value])
-        epoch = np.array([epoch])
-        if epoch == 0:
-            self.viz_metric = self.window.line(Y=value, X=epoch, env=self.env, opts=dict(xlabel=xlabel, ylabel=ylabel, title=title))
-        else:
-            self.window.updateTrace(Y=value, X=epoch, env=self.env, win=self.viz_metric)
-
-    # def heatmap_plot():
-
-
-class Log(object):
-    def __init__(self, model_type):
-        model_type = str(model_type)
-        self.log_type = logging.getLogger(model_type)
-        
-    def log_metric(self, statement):
-            self.log_type.info(statement)
 
